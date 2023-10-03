7eace11c786fa3ff85b3658eda309098a8ec22c3
Dirk Olbrich <mail@dirkolbrich.de>
2023-10-03 21:27:27 +0200

    delete src/bcf folder

--- src/bcf/src/bcf/v3/__init__.py
+++
@@ -1,1 +0,0 @@
-"""BCF XML v3 handler."""
--- src/bcf/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from bcf.xml_parser import XmlParserSerializer
-
-
-@pytest.fixture(scope="session")
-def xml_handler() -> XmlParserSerializer:
-    return XmlParserSerializer()
